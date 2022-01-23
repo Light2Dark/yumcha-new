00b5862b9edc48037372590ec6e7c0b91255a414
Shahmir Varqha <Sham9871@gmail.com>
2022-01-23 23:55:21 +0800

    initial web page to develop on!

---
+++ components/Header/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import styles from "./nav.module.css"
+
+export default function Navbar() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.heading}>Yumcha</h1>
+        </div>
+    )
+}
