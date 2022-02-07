14a966fc63f1d638a91733ef6acc3ef843798793
Shahmir Varqha <Sham9871@gmail.com>
2022-02-07 13:37:39 +0800

    working db!

--- components/Main/YumchaCard/yumchaCard.tsx
+++ components/Main/YumchaCard/yumchaCard.tsx
@@ -2,18 +2,29 @@ import styles from "./yum.module.css"
 import Image from "next/image"
 import face from "../../../public/images/profileface.png"
 
-const Card = () => {
+export interface YumchaProps {
+    id?: number;
+    inserted_at?: String;
+    username: String;
+    yumchaTitle: String;
+    place: String;
+    time: String;
+    description: String;
+    phoneNum: String;
+}
+
+const Card = ({username, yumchaTitle, time, description, phoneNum} : YumchaProps) => {
     return(
         <>
             <div className={styles.f2fyumcha}>
                 <div className={styles.visible}>
-                    <span><b>Alex</b></span>
-                    <span className={styles.title}>Dinner + D&D<span className = {styles.time}>12.30pm</span></span>
+                    <span><b>{username}</b></span>
+                    <span className={styles.title}>{yumchaTitle}<span className = {styles.time}>1{time}</span></span>
                     {/* <div className = {styles.expandBtn} onClick = {() => console.log("expandF2FYumcha(this)")}><b>&#62;</b></div> */}
                 </div>
                 <div className={styles.expanded}>
                     <Image src={face} alt="Face of person" className={styles.f2fimg} height={"45px"} width={"45px"}  />
-                    <span className={styles.text}>I love Warlord! I'm behind the sign.</span>
+                    <span className={styles.text}>{description}</span>
                     <span className={styles.join}><a href="#">Join</a></span>
                 </div>
             </div>
--- pages/yumchaNow.tsx
+++ pages/yumchaNow.tsx
@@ -3,13 +3,98 @@ import Head from "next/head"
 import Navbar from "../components/Header/Navbar/Navbar"
 import MainNav from "../components/Header/MainNav/MainNav"
 import YumchaCard from "../components/Main/YumchaCard/yumchaCard"
+import { YumchaProps } from "../components/Main/YumchaCard/yumchaCard"
 import mainStyles from "../styles/main.module.css"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { supabase } from "../utils/supabaseClient"
 
-const Page: NextPage = () => {
+type Props = {
+    session: any;
+}
 
-    const [yumchas, setYumchas] = useState<typeof YumchaCard[]>([])
+// const Yumcha = ({session}: Props) => {
+    
+
+//     return (
+//         <>
+
+//         </>
+//     )
+// }
+
+const Page = ({session}: Props) => {
+    // const [yumchas, setYumchas] = useState<typeof YumchaCard[]>([])
+    const [loading, setLoading] = useState(false)
+    const [yumchas, setYumchas] = useState<Array<typeof YumchaCard>>([])
+
+    useEffect(() => {
+        GetYumchas()
+    }, [session])
+    
+
+    async function GetYumchas() {
+        try {
+            setLoading(true)
+            let {data, error, status} = await supabase
+                .from("yumcha")
+                .select()
+
+            // let { data: yumcha, error } = await supabase
+            //     .from('yumcha')
+            //     .select('*')
+                      
+            
+            if (error && status !== 406) {
+                console.log("error not 406")
+                throw error
+            }
+
+            if (data) {
+                setYumchas(data)
+                console.log(yumchas)
+            }
+
+        } catch(error) {
+            console.error(error)
+
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    async function AddYumcha() {
+        try {
+            setLoading(true)
+            let {data: yumcha, error, status} = await supabase
+                .from("yumcha")
+                .insert([
+                    {username: "Shahmirasd"}
+                ])
+
+            
+            // let { data: yumcha, error } = await supabase
+            //     .from('yumcha')
+            //     .select('*')
+                      
+            
+            if (error && status !== 406) {
+                console.log("error not 406")
+                throw error
+            }
+
+            if (yumcha) {
+                console.log("yumcha", yumcha)
+            }
+
+        } catch(error) {
+            console.error(error)
+
+        } finally {
+            setLoading(false)
+        }
+    }
+
 
     return(
         <>
@@ -29,13 +114,16 @@ const Page: NextPage = () => {
                 </div> */}
                 <div className={mainStyles.card}>
                     <h3>Current Yumchas:</h3>
-                    <YumchaCard />
-                    <YumchaCard />
+                    {yumchas.map(({description, phoneNum, place, time, username, yumchaTitle, id}: YumchaProps) => {
+                        return(
+                            <YumchaCard description={description} phoneNum={phoneNum} place={place} time={time} username={username} yumchaTitle={yumchaTitle} key={id} />
+                        )
+                    })}
 
                     <h3>Upcoming Yumchas:</h3>
-                    <YumchaCard />
+                    {/* <YumchaCard /> */}
                 </div>
-                
+                <button className={mainStyles.button} onClick={() => {}}>Get Yumcha</button>
 
             </main>
         </>
