a3b4703b0ac3ec6c5822e3dce9fa607c543e60d2
Shahmir Varqha <Sham9871@gmail.com>
2022-02-09 18:14:41 +0800

    adding google maps integration

--- pages/planYumcha.tsx
+++ pages/planYumcha.tsx
@@ -5,6 +5,34 @@ import MainNav from "../components/Header/MainNav/MainNav"
 import mainStyles from "../styles/main.module.css"
 import YumchaForm from "../components/Main/YumchaForm/yumchaForm"
 
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { useRef, useState, useEffect } from "react"
+const API_KEY: string = process.env.NEXT_PUBLIC_MAPS_API!
+
+const render = (status: Status) => {
+    return <h1>{status}</h1>
+}
+
+const Map: React.FC<{}> = () => {
+    const ref = useRef<HTMLDivElement>(null)
+    const [map, setMap] = useState<google.maps.Map>()
+
+    useEffect(() => {
+      if(ref.current && !map) {
+          setMap(new window.google.maps.Map(ref.current, {}))
+      }
+    }, [ref, map]);
+    
+
+    return(
+        <>
+            <div ref={ref}>
+               
+            </div> 
+        </>
+    )
+}
+
 const Page: NextPage = () => {
     return(
         <>
@@ -19,9 +47,12 @@ const Page: NextPage = () => {
 
             <main>
                 <MainNav currentPage="Plan Yumcha" />
-                {/* <div id ="map">
-                     
-                </div> */}
+
+                <Wrapper apiKey={API_KEY}>
+                    <Map />
+                    <h2>Hello</h2>
+                </Wrapper>
+
                 <div>
                     <YumchaForm />
                 </div>
