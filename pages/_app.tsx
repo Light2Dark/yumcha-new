3af630222f7807b188b88210fd1c5e82b7e5509f
Shahmir Varqha <Sham9871@gmail.com>
2022-02-26 22:10:34 +0800

    changed script location for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,30 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      
+      <script
+          dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+      `,
+          }}
+      />
+
+        <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
-import Script from 'next/script'
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
@@ -12,24 +11,6 @@ class MyDocument extends Document {
         return (
         <Html>
             <Head>
-                {/* Global Site Tag (gtag.js) - Google Analytics */}
-                <Script
-                    async
-                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                />
-                <Script
-                    dangerouslySetInnerHTML={{
-                    __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `,
-                    }}
-                />
-
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200;300;400;500;600;700;800&family=Shadows+Into+Light&display=swap" rel="stylesheet" />
